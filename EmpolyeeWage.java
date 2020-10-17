0006710e38fdcdea77879c645e9a8a971d4a8330
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-17 11:03:04 +0530

    Daily wage calculation of the employees

--- EmpolyeeWage.java
+++ EmpolyeeWage.java
@@ -1,9 +1,25 @@
 package empwage;
 
 public class EmpolyeeWage {
-
+	final static float WAGE_PER_HR=20;
+	final static float FULL_DAY_HR=8;
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Employee Wage Computation");
+		
+		float dailywage;
+		double emprandom=Math.floor(Math.random() * 10)%2;
+		
+		if (emprandom == 1) {
+			System.out.println("Employee is present");
+			dailywage=WAGE_PER_HR*FULL_DAY_HR;
+			System.out.println("Dailywage of the Employee="+dailywage);
+		}
+		else {
+			System.out.println("Employee is absent");
+			dailywage=0; 
+			System.out.println("Dailywage of the Employee="+dailywage);
+		}
+		
 	}
 
 }

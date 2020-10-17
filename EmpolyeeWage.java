f46b5ddb195196789296ffbe08b9219c54eabbd5
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-17 11:47:08 +0530

    Monthly wage calculation of the employee

--- EmpolyeeWage.java
+++ EmpolyeeWage.java
@@ -4,6 +4,7 @@ public class EmpolyeeWage {
 	final static float WAGE_PER_HR=20;
 	final static float FULL_DAY_HR=8;
 	final static float PART_TIME_HR=4;
+	final static float MONTHLY_DAYS=20;
 	
 	public static void main(String[] args) {
 		
@@ -14,13 +15,13 @@ public class EmpolyeeWage {
 		switch (emp){
 		case 1:
 			System.out.println("Employee is Full time present");
-			dailywage=WAGE_PER_HR*FULL_DAY_HR;
+			dailywage=WAGE_PER_HR*FULL_DAY_HR*MONTHLY_DAYS;
 			System.out.println("Dailywage of the Employee= "+dailywage);
 			break;
 		
 		case 2:
 			System.out.println("Employee is Part time Present");
-			dailywage=WAGE_PER_HR*PART_TIME_HR;
+			dailywage=WAGE_PER_HR*PART_TIME_HR*MONTHLY_DAYS;
 			System.out.println("Part time wages of the Employee= "+dailywage);
 			break;
 		

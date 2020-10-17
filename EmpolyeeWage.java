125e525b2262287eefa393f01243edeab48a7ba1
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-17 12:31:31 +0530

    Using classes and objects and calculating wages

--- EmpolyeeWage.java
+++ EmpolyeeWage.java
@@ -1,39 +1,56 @@
 package empwage;
 
-public class EmpolyeeWage {
-	final static float WAGE_PER_HR=20;
-	final static float FULL_DAY_HR=8;
-	final static float PART_TIME_HR=4;
-	final static float MONTHLY_DAYS=20;
+class Emprst{
 	
+	float WAGE_PER_HR=20;
+	float FULL_DAY_HR=8;
+	float PART_TIME_HR=4;
+	float MONTHLY_DAYS=20;
+	float working_day=0;
+	float working_hr=0;
+	float max_hr=100;
+	float num_working_days=20;
+	float dailywage;
+	float emphr;
+	
+
+	void checkval() { 
+		while (working_hr <= max_hr && working_day <= num_working_days) {
+			working_day++;
+			
+			double emprandom=Math.floor(Math.random() * 10)%3;
+			int emp= (int) emprandom;
+			switch (emp){
+			case 1:
+				emphr=8;
+				break;
+	
+			case 2:
+				emphr=4;
+				break;
+	
+			case 0:
+				emphr=0;
+				break;
+	
+			default:
+				System.out.println("Error");
+	
+			}
+		working_hr=working_hr+emphr;
+		}
+	float wage=working_hr*WAGE_PER_HR;
+	System.out.println("Wages= "+wage);
+	}
+}
+
+public class EmpolyeeWage {
+		
 	public static void main(String[] args) {
 		
-		float dailywage;
-		double emprandom=Math.floor(Math.random() * 10)%3;
-		int emp= (int) emprandom;
-		
-		switch (emp){
-		case 1:
-			System.out.println("Employee is Full time present");
-			dailywage=WAGE_PER_HR*FULL_DAY_HR*MONTHLY_DAYS;
-			System.out.println("Dailywage of the Employee= "+dailywage);
-			break;
-		
-		case 2:
-			System.out.println("Employee is Part time Present");
-			dailywage=WAGE_PER_HR*PART_TIME_HR*MONTHLY_DAYS;
-			System.out.println("Part time wages of the Employee= "+dailywage);
-			break;
-		
-		case 0:
-			System.out.println("Employee is absent");
-			dailywage=0; 
-			System.out.println("Dailywage of the Employee= "+dailywage);
-			break;
-		
-		default:
-			System.out.println("Error");
-		}
-		
+		Emprst e1= new Emprst();
+		Emprst e2= new Emprst();
+		e1.checkval();
+		e2.checkval();
 	}
 }

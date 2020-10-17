378dc833054c04620ba33318d97c2a24abdb056f
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-17 10:40:22 +0530

    Prints the Welcome Message

--- EmpolyeeWage.java
+++ EmpolyeeWage.java
@@ -3,13 +3,7 @@ package empwage;
 public class EmpolyeeWage {
 
 	public static void main(String[] args) {
-		double emprandom=Math.floor(Math.random() * 10)%2;
-		System.out.println(emprandom);
-		if (emprandom == 1)
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent");
-
+		System.out.println("Welcome to the Employee Wage Computation");
 	}
 
 }

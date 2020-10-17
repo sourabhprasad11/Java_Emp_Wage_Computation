3a6d7093c524a6e38df1a1c42227f182f3fd00f2
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-17 11:17:56 +0530

    Part-Time Wages of the Employee calculated

--- EmpolyeeWage.java
+++ EmpolyeeWage.java
@@ -3,21 +3,27 @@ package empwage;
 public class EmpolyeeWage {
 	final static float WAGE_PER_HR=20;
 	final static float FULL_DAY_HR=8;
+	final static float PART_TIME_HR=4;
 	
 	public static void main(String[] args) {
 		
 		float dailywage;
-		double emprandom=Math.floor(Math.random() * 10)%2;
+		double emprandom=Math.floor(Math.random() * 10)%3;
 		
 		if (emprandom == 1) {
-			System.out.println("Employee is present");
+			System.out.println("Employee is Full time present");
 			dailywage=WAGE_PER_HR*FULL_DAY_HR;
-			System.out.println("Dailywage of the Employee="+dailywage);
+			System.out.println("Dailywage of the Employee= "+dailywage);
+		}
+		else if (emprandom == 2) {
+			System.out.println("Employee is Part time Present");
+			dailywage=WAGE_PER_HR*PART_TIME_HR;
+			System.out.println("Part time wages of the Employee= "+dailywage);
 		}
 		else {
 			System.out.println("Employee is absent");
 			dailywage=0; 
-			System.out.println("Dailywage of the Employee="+dailywage);
+			System.out.println("Dailywage of the Employee= "+dailywage);
 		}
 		
 	}
